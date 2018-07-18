d24d6b5100b80b56c6d2f99145e250defc292903
nanobeauty <nanobeauty@naver.com>
2018-07-19 01:15:26 +0700

    1.http://localhost:3000/api/posts 에서 json 형태의 데이터를 Angular Service file을 이용해서 API를 호출하는 형식으로 해서 localhost:3000 에서 바로 가공된 데이터 형태로 보여준다
    2. ng generate service Post로
    post.service.ts 파일 생성하고
    그 안에서 getPosts라는 함수 생성
    
    3. app.module.ts 파일에 PostService 를 import 한다.
    
    4. ng g c nav
        로 nav component 생성 후에
        /src/app 안의 app.component.html 파일안에
       <app-nav></app-nav> 위치 지정해주고
    
    5. root 의 index.html에
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation-float.min.css">
        foundation css cdn를 연결시켜준다
    
    6. foundation css의 영향아래에서
        nav.component.html 과 그 이후의 home.component.html 파일을 작성한다.
    
    7. ng g c Home 으로
        home  Component 생성 후에
        ng g class Post 로
        Post 클래스 생성 후에  id, title, url, description type 을 정해준다.
    
        home.component에서 Post 클래스를 import  하고, posts collection의 타입을 array(?)로 지정후에
        post.service.ts 화일 속의 getPosts 메서드를 불러와 배열로 뿌려준다.
    
    8. home.component.html 파일에서
        *ngFor="let post of posts"로 DB를 뿌려주고,
    
        {{ post.title }}     {{ post.description }}
        로 보여준다
    
    9. app-routing.module.ts 파일에서
        HomeComponent를 import 하고 path를 지정해준다.
    
    10. ng build해주고 node server 해주고
          localhost:3000 해주면, DB의 데이터를 json 형태가 아니라 일반적인 형태로 볼 수 잇다.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostService } from './post.service';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
